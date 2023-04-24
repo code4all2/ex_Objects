b5dd7f59d68ce06213c060160da223e134254e5e
code4all2 <code4allaulaonline@gmail.com>
2023-04-24 14:02:53 -0300

    ex object

---
+++ js/main.js
@@ -0,0 +1,51 @@
+function pegarInfos() {
+
+    const form = document.querySelector('.form');
+
+    const resultado = document.querySelector('.resultado');
+
+    const pessoa = [];
+
+    // form.onsubmit = function(evento){
+    //     evento.preventDefault();
+    // }
+
+    function recebeEvento(evento) {
+        evento.preventDeafault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+    }
+
+
+    /**
+     * RESOLUÇÃO AQUI EMBAIXO
+     */
+    pessoa.push({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        peso: peso.value,
+        altura: altura.value
+    });
+    console.log(pessoa);
+
+    /**
+     * 
+     * Imprime na div
+     */
+
+    resultado.innerHTML += ` 
+    <p> ${nome.value} </p>
+    <p> ${sobrenome.value} </p>
+    <p> ${peso.value} </p>
+    <p> ${altura.value} </p>
+
+    `
+
+    form.addEventListener('submit', recebeEvento);
+
+}
+
+pegarInfos();
